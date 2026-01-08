c97caa33cce1d633a9d65fed267f3399ba2064a8
janitha325 <janithachanak@gmail.com>
2026-01-08 13:12:52 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𝐉𝐀𝐍𝐈𝐘𝐀 𝐕𝟏𝟐',
+  author: '‎',
+  botName: "𝐉𝐀𝐍𝐈𝐘𝐀 𝐕𝟏𝟐",
+  botOwner: 'Professor', // Your name
+  ownerNumber: '94766960038', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "12",
+  updateZipUrl: "https://github.com/janitha325/-..git",
+};
+
+module.exports = settings;
